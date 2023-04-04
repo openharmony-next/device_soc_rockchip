0f3a6efbf2841b91f1a4027172b45ed27194bd19
libing_frank <libing3@huawei.com>
2023-04-04 18:22:03 +0800

    fix screen ficker
    Signed-off-by: libing_frank <libing3@huawei.com>
    Change-Id: I6d849de65bf644b845b14a3bb9e1642903208d91

--- rk3568/hardware/display/src/display_device/hdi_drm_composition.cpp
+++ rk3568/hardware/display/src/display_device/hdi_drm_composition.cpp
@@ -16,7 +16,6 @@
 #include "hdi_drm_composition.h"
 #include <cerrno>
 #include "hdi_drm_layer.h"
-#include <libsync.h>
 
 namespace OHOS {
 namespace HDI {
@@ -309,7 +308,6 @@ int32_t HdiDrmComposition::Apply(bool modeSet)
     DISPLAY_CHK_RETURN((ret != 0), DISPLAY_FAILURE,
         DISPLAY_LOGE("drmModeAtomicCommit failed %{public}d errno %{public}d", ret, errno));
     // set the release fence
-    sync_wait(crtcOutFence, 3000);  // 3000: wait time
     for (auto layer : mCompLayers) {
         layer->SetReleaseFence(static_cast<int>(crtcOutFence));
     }
